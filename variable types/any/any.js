83672cf775728b523eff369ab361b4c9aa22885a
ShreyasGeetha <shreyasgeetha@gmail.com>
2022-03-05 17:31:27 +0530

    types in typescript

--- variable types/any/any.js
+++ variable types/any/any.js
@@ -7,9 +7,9 @@ number = true;
 number = 'hello';
 var anyVariable = 10;
 // this does not throw an error because of any type. But the purpose of typescript is defeated here
-anyVariable.toUpperCase();
-anyVariable.name;
-anyVariable();
+// anyVariable.toUpperCase()
+// anyVariable.name
+// anyVariable()
 /**
  * to esnure we cannot call any property types, call or construct them - we have unknown type
  */
@@ -17,7 +17,7 @@ var a = 10;
 //below code would throw error
 // a.toUpperCase()
 // correct way of declaring is
-a.toUpperCase();
+// (a as string).toUpperCase();
 function hasName(obj) {
     return !!obj &&
         typeof obj === "object" && "name" in obj;
@@ -26,3 +26,26 @@ function hasName(obj) {
 if (hasName(a)) {
     console.log(a.name);
 }
+var alpha;
+alpha = 10;
+alpha = true;
+var beta = 10;
+//statement below will cause an error because, it has already been assigned a number at declaration and now by default beta variable is considered as number type in line 46
+// beta = false;
+// variable having multiple types
+//the below variable can accept two values either boolean or number
+var multitype;
+multitype = 10;
+console.log(multitype);
+multitype = false;
+console.log(multitype);
+//if we try to assign string to multitype - it throws an error
+// multitype = 'hello'
+/**
+ * the difference between any and multitype is
+ * in multitype you explicitly mention what types can be assigned to those variables
+ *
+ * in any - all supported types can be used for that variable.
+ *
+ * even with respect to intellisense, with multitype is shows options only with respected to the latest assigned type
+ */
--- variable types/any/any.ts
+++ variable types/any/any.ts
@@ -11,9 +11,9 @@ number = 'hello'
 let anyVariable: any = 10
 
 // this does not throw an error because of any type. But the purpose of typescript is defeated here
-anyVariable.toUpperCase()
-anyVariable.name
-anyVariable()
+// anyVariable.toUpperCase()
+// anyVariable.name
+// anyVariable()
 
 /**
  * to esnure we cannot call any property types, call or construct them - we have unknown type
@@ -25,7 +25,7 @@ let a: unknown = 10;
 // a.toUpperCase()
 
 // correct way of declaring is
-(a as string).toUpperCase();
+// (a as string).toUpperCase();
 
 function hasName(obj: any): obj is { name: string }{
   return !!obj && 
@@ -36,3 +36,34 @@ function hasName(obj: any): obj is { name: string }{
 if (hasName(a)) {  
   console.log(a.name)
 }
+
+let alpha;
+alpha = 10;
+alpha = true;
+
+let beta = 10;
+//statement below will cause an error because, it has already been assigned a number at declaration and now by default beta variable is considered as number type in line 46
+// beta = false;
+
+// variable having multiple types
+
+//the below variable can accept two values either boolean or number
+let multitype: boolean | number;
+multitype = 10
+console.log(multitype)
+multitype = false
+console.log(multitype)
+
+//if we try to assign string to multitype - it throws an error
+// multitype = 'hello'
+
+/**
+ * the difference between any and multitype is
+ * in multitype you explicitly mention what types can be assigned to those variables
+ * 
+ * in any - all supported types can be used for that variable.
+ * 
+ * even with respect to intellisense, with multitype is shows options only with respected to the latest assigned type
+ */
+
+
